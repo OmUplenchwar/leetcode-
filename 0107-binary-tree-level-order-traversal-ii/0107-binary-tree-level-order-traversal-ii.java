8da57602c8168486c9739f417676f75125887eff
OmUplenchwar <omuplenchwar0401@gmail.com>
2026-07-22 05:08:04 +0530

    Time: 1 ms (97.23%), Space: 43.8 MB (99.57%) - LeetHub - Time: O(N), Space: O(N)

---
+++ 0107-binary-tree-level-order-traversal-ii/0107-binary-tree-level-order-traversal-ii.java
@@ -0,0 +1,50 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public List<List<Integer>> levelOrderBottom(TreeNode root) {
+        List<List<Integer>> ans=new ArrayList<>();
+        if(root==null){
+            return ans;
+        }
+        Queue<TreeNode> q=new LinkedList<>();
+        q.add(root);
+        while(!q.isEmpty()){
+            List<TreeNode> temp=new ArrayList<>();
+            List<Integer> demo=new ArrayList<>();
+            while(!q.isEmpty()){
+                TreeNode node=q.poll();
+               temp.add(node);
+               demo.add(node.val);
+            }
+            ans.add(demo);
+            for(int i=0;i<temp.size();i++){
+                TreeNode node=temp.get(i);
+                if(node.left!=null){
+                    q.add(node.left);
+                }
+                if(node.right!=null){
+                    q.add(node.right);
+                }
+            }
+        }
+        Collections.reverse(ans);
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
