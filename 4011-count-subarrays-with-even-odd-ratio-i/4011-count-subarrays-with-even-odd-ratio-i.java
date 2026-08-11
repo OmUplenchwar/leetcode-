adcd0d2d31b8ae0e3b91538a899ca61ee04ebb45
OmUplenchwar <omuplenchwar0401@gmail.com>
2026-08-11 17:43:36 +0530

    Time: 95 ms (5.11%), Space: 46.4 MB (83.95%) - LeetHub - Time: O(n^2), Space: O(1)

---
+++ 4011-count-subarrays-with-even-odd-ratio-i/4011-count-subarrays-with-even-odd-ratio-i.java
@@ -0,0 +1,22 @@
+class Solution {
+    public int countRatioSubarrays(int[] nums, int a, int b) {
+        int ans=0;
+        for(int i=0;i<nums.length;i++){
+            int x=0;
+            int y=0;
+            for(int j=i;j<nums.length;j++){
+                if(nums[j]%2==0) x++;
+                else y++;
+                if(y>0 && ((double)x/(double)y)<=((double)a/(double)b)){
+                    ans++;
+                    // System.out.println(i+""+j);
+                }
+            }
+        }
+        return ans;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
