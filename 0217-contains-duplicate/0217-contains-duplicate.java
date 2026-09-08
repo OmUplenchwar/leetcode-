c78f3f0ca076aaa0a9ee21c70c4b362da5c65b22
OmUplenchwar <omuplenchwar0401@gmail.com>
2026-09-09 01:29:40 +0530

    Time: 18 ms (77.06%), Space: 107.4 MB (70.57%) - LeetHub - Time: O(n), Space: O(n)

---
+++ 0217-contains-duplicate/0217-contains-duplicate.java
@@ -0,0 +1,14 @@
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+        HashSet<Integer> set=new HashSet<>();
+        for(int i=0;i<nums.length;i++){
+            if(set.contains(nums[i])) return true;
+            set.add(nums[i]);
+        }
+        return false;
+    }
+}
+
+// Synced seamlessly with LeetHub Pro
+// Pro features: https://bit.ly/leethubpro | Free version: https://bit.ly/leethubv4
+// Get it here: https://chromewebstore.google.com/detail/bcilpkkbokcopmabingnndookdogmbna
